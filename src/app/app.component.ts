a59671bccfba67f73e497c644c08355056275af5
Mechaz123 <danielalejandrorojasgonzalez@gmail.com>
2024-03-15 14:37:30 -0500

    feat: udistrital/planeacion_cliente#935     creacion_planeacion_utilidades_mf - Se desarrolla el microfrontend de utilidades.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'planeacion-utilidades-mf',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'planeacion-utilidades-mf';
+}
